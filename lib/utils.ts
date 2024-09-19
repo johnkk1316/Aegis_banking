e3b45375d46c740889522e03509c4cc2154067a7
johnkk1316 <codeona4@gmail.com>
2024-09-19 17:08:00 +0300

    impliment Appwrite authentication & Securing app with sentry

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,7 +1,9 @@
+
 /* eslint-disable no-prototype-builtins */
 import { type ClassValue, clsx } from "clsx";
 import qs from "query-string";
 import { twMerge } from "tailwind-merge";
+import { z } from 'zod';
 
 export function cn(...inputs: ClassValue[]) {
     return twMerge(clsx(inputs));
@@ -191,3 +193,20 @@ export const getTransactionStatus = (date: Date) => {
 
     return date > twoDaysAgo ? "Processing" : "Success";
 };
+
+export const authFormSchema = (type: string) => z.object({
+
+	// Sign Up
+	firstName: type === 'sign-in' ? z.string().optional() : z.string().min(3),
+	lastName: type === 'sign-in' ? z.string().optional() : z.string().min(3),
+	address1: type === 'sign-in' ? z.string().optional() : z.string().max(50),
+	city: type === 'sign-in' ? z.string().optional() : z.string().max(50),
+	state: type === 'sign-in' ? z.string().optional() : z.string().min(2).max(20),
+	postalCode: type === 'sign-in' ? z.string().optional() : z.string().min(5).max(5),
+	dateOfBirth: type === 'sign-in' ? z.string().optional() : z.string().min(10).max(10),
+	idNumber: type === 'sign-in' ? z.string().optional() : z.string().min(8).max(8),
+
+	// Both
+	email: z.string().email(),
+	password: z.string().min(8),
+})
